0ce145436b66c592419f69c2cdea7310e8b9a5b1
George A. <sg1asgard@outlook.com>
2024-09-19 17:05:13 +0300

    stage 1

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,7 +7,7 @@ import TopPart from '@/components/layout/TopPart.vue'
   <div>
     <TopPart />
     <div>
-      <div class="max-w-8xl mx-auto px-4 sm:px-6 md:px-8 bg-zinc-100">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 bg-zinc-100">
         <AsidePart />
         <div class="lg:pl-[19.5rem] bg-slate-100">
           <div class="max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16">
