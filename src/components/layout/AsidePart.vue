184f51ee4e2e5845b857f5fd1b1d9f4ada18e297
George A. <sg1asgard@outlook.com>
2024-09-19 17:48:49 +0300

    stage 1

--- src/components/layout/AsidePart.vue
+++ src/components/layout/AsidePart.vue
@@ -1,21 +1,38 @@
 <script setup>
 import { RouterLink } from 'vue-router'
 import HeaderPart from './aside/HeaderPart.vue'
+import LinkPart from './aside/LinkPart.vue'
 </script>
 <template>
   <div
     class="hidden lg:block fixed z-20 inset-0 top-[3.8125rem] left-[max(0px,calc(50%-40rem))] right-auto w-[19rem] pb-10 pl-8 pr-6 overflow-y-auto border-r-[1px]"
   >
     <nav class="lg:text-sm lg:leading-6 relative">
-      <ul>
+      <ul class="text-slate-700">
         <li class="mt-12 lg:mt-8">
           <section class="flex flex-col">
             <HeaderPart>Getting started</HeaderPart>
-            <RouterLink :to="{ name: 'home' }">Introduction</RouterLink>
-            <RouterLink :to="{ name: 'home' }">Quick Start</RouterLink>
+            <RouterLink :to="{ name: 'home' }"><LinkPart>Introduction</LinkPart></RouterLink>
+            <RouterLink :to="{ name: 'home' }"><LinkPart>Quick Start</LinkPart></RouterLink>
+          </section>
+        </li>
+
+        <li class="mt-12 lg:mt-8">
+          <section class="flex flex-col">
+            <HeaderPart>Components</HeaderPart>
+            <RouterLink :to="{ name: 'home' }"><LinkPart>Alert</LinkPart></RouterLink>
+            <RouterLink :to="{ name: 'home' }"><LinkPart>Button</LinkPart></RouterLink>
+            <RouterLink :to="{ name: 'home' }"><LinkPart>Card</LinkPart></RouterLink>
           </section>
         </li>
       </ul>
     </nav>
   </div>
 </template>
+
+<style lang="css" scoped>
+a {
+  font-size: 18px;
+  padding: 4px 0px;
+}
+</style>
