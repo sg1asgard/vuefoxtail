9a5af8eaa6a3103cc004dbc0e0fb22f652fbe6c8
George A. <sg1asgard@outlook.com>
2024-09-19 17:33:06 +0300

    stage 1

--- src/components/layout/AsidePart.vue
+++ src/components/layout/AsidePart.vue
@@ -7,13 +7,15 @@ import HeaderPart from './aside/HeaderPart.vue'
     class="hidden lg:block fixed z-20 inset-0 top-[3.8125rem] left-[max(0px,calc(50%-40rem))] right-auto w-[19rem] pb-10 pl-8 pr-6 overflow-y-auto border-r-[1px]"
   >
     <nav class="lg:text-sm lg:leading-6 relative">
-      <div class="group">
-        <section class="flex flex-col">
-          <HeaderPart>Getting started</HeaderPart>
-          <RouterLink :to="{ name: 'home' }">Introduction</RouterLink>
-          <RouterLink :to="{ name: 'home' }">Quick Start</RouterLink>
-        </section>
-      </div>
+      <ul>
+        <li class="mt-12 lg:mt-8">
+          <section class="flex flex-col">
+            <HeaderPart>Getting started</HeaderPart>
+            <RouterLink :to="{ name: 'home' }">Introduction</RouterLink>
+            <RouterLink :to="{ name: 'home' }">Quick Start</RouterLink>
+          </section>
+        </li>
+      </ul>
     </nav>
   </div>
 </template>
