2b73fc433dd9ac23c604167a772697ac76063017
George A. <sg1asgard@outlook.com>
2024-09-19 18:11:38 +0300

    stage 1

---
+++ src/components/vuefoxtail/alert/AlertVFX.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <div class="">Alert</div>
+</template>
--- src/views/components/AlertView.vue
+++ src/views/components/AlertView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import MainPart from '@/components/layout/MainPart.vue'
+import AlertVFX from '@/components/vuefoxtail/alert/AlertVFX.vue'
 import MainLayout from '@/layouts/MainLayout.vue'
 </script>
 
@@ -7,6 +8,10 @@ import MainLayout from '@/layouts/MainLayout.vue'
   <MainLayout>
     <MainPart>
       <h1 class="text-3xl font-bold underline">Alerts</h1>
+
+      <div class="mt-12">
+        <AlertVFX />
+      </div>
     </MainPart>
   </MainLayout>
 </template>
