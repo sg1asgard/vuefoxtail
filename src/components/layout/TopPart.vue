98f423ec2497ff96d54ef200a9398913c01bd822
George A. <sg1asgard@outlook.com>
2024-09-19 17:18:09 +0300

    stage 1

--- src/components/layout/TopPart.vue
+++ src/components/layout/TopPart.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="sticky top-0 z-40 w-full flex-none border-b-[1px] border-slate-900/20">
     <div class="max-w-7xl mx-auto">
-      <div class="py-4">VueFoxTail</div>
+      <div
+        class="py-4 border-b border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0"
+      >
+        VueFoxTail
+      </div>
     </div>
   </div>
 </template>
