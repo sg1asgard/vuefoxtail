a4c4b68fcfaebabb0b3322c02df3f6ae02245d8e
George A. <sg1asgard@outlook.com>
2024-09-19 18:19:18 +0300

    stage 1

--- src/components/vuefoxtail/alert/AlertVFX.vue
+++ src/components/vuefoxtail/alert/AlertVFX.vue
@@ -1,5 +1,13 @@
 <script setup></script>
 
 <template>
-  <div class="">Alert</div>
+  <div class="border-2 border-indigo-300 p-4 bg-indigo-200 rounded-md">Alert</div>
+
+  <div class="border-2 border-teal-300 p-4 bg-teal-200 rounded-md">Alert</div>
+
+  <div class="border-2 border-red-200 p-4 bg-red-100 rounded-md">Alert</div>
+
+  <div class="border-2 border-orange-200 p-4 bg-orange-100 rounded-md">Alert</div>
+
+  <div class="border-2 border-sky-200 p-4 bg-sky-100 rounded-md">Alert</div>
 </template>
