c4b4ca7aa0167539bc534be0529b6d6751ee5ba1
George A. <sg1asgard@outlook.com>
2024-09-20 13:15:13 +0300

    buttons

--- src/components/vuefoxtail/forms/buttons/ButtonVFX.vue
+++ src/components/vuefoxtail/forms/buttons/ButtonVFX.vue
@@ -18,7 +18,7 @@ const btnLarge = ref(
       <button
         class="bg-indigo-200 hover:bg-indigo-300/75 rounded-md px-2.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"
       >
-        Button
+        Small Button
       </button>
     </div>
 
@@ -26,7 +26,7 @@ const btnLarge = ref(
       <button
         class="bg-indigo-200 hover:bg-indigo-300/75 rounded-md px-3 py-2 hover:shadow-lg hover:shadow-black/25"
       >
-        Button
+        Medium Button
       </button>
     </div>
 
@@ -34,15 +34,52 @@ const btnLarge = ref(
       <button
         class="bg-indigo-200 hover:bg-indigo-300/75 rounded-md px-4 py-3 text-xl hover:shadow-lg hover:shadow-black/25"
       >
-        Button
+        Large Button
       </button>
     </div>
   </div>
 
-  <div class="py-4">
-    <highlightjs autodetect :code="btnSmall" />
-    <highlightjs autodetect :code="btnMedium" />
-    <highlightjs autodetect :code="btnLarge" />
+  <div class="py-4 flex flex-col space-y-6">
+    <div>
+      <span>Small</span>
+      <highlightjs autodetect :code="btnSmall" />
+    </div>
+
+    <div>
+      <span>Medium</span>
+      <highlightjs autodetect :code="btnMedium" />
+    </div>
+
+    <div>
+      <span>Large</span>
+      <highlightjs autodetect :code="btnLarge" />
+    </div>
+  </div>
+
+  <div class="mt-16 flex flex-row space-x-6 items-center">
+    <div>
+      <button
+        class="bg-indigo-200 hover:bg-indigo-300/75 rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"
+      >
+        Button
+      </button>
+    </div>
+
+    <div>
+      <button
+        class="bg-indigo-200 hover:bg-indigo-300/75 rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25"
+      >
+        Button
+      </button>
+    </div>
+
+    <div>
+      <button
+        class="bg-indigo-200 hover:bg-indigo-300/75 rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25"
+      >
+        Button
+      </button>
+    </div>
   </div>
 </template>
 <style lang="css" scoped></style>
