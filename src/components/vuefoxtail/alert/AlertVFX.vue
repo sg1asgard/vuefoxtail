408dd20749bc9b8b3f854ba665bf6c7522335b13
George A. <sg1asgard@outlook.com>
2024-09-20 09:45:14 +0300

    updates

--- src/components/vuefoxtail/alert/AlertVFX.vue
+++ src/components/vuefoxtail/alert/AlertVFX.vue
@@ -1,6 +1,15 @@
-<script setup></script>
+<script setup>
+import hljs from 'highlight.js'
+import javascript from 'highlight.js/lib/languages/javascript'
+
+hljs.registerLanguage('javascript', javascript)
+
+const highlightedCode = hljs.highlight('<span>Hello World!</span>', { language: 'xml' }).value
+</script>
 
 <template>
+  {{ highlightedCode }}
+
   <div class="border-2 border-indigo-300 p-4 bg-indigo-200 rounded-md">Alert</div>
 
   <div class="border-2 border-teal-300 p-4 bg-teal-200 rounded-md">Alert</div>
@@ -11,3 +20,32 @@
 
   <div class="border-2 border-sky-200 p-4 bg-sky-100 rounded-md">Alert</div>
 </template>
+
+<style lang="css" scoped>
+pre {
+  display: block;
+  font-family: monospace;
+  unicode-bidi: isolate;
+  white-space: pre;
+  margin: 1em 0px;
+}
+
+code,
+kbd,
+pre,
+samp {
+  font-family:
+    Fira Code VF,
+    ui-monospace,
+    SFMono-Regular,
+    Menlo,
+    Monaco,
+    Consolas,
+    Liberation Mono,
+    Courier New,
+    monospace;
+  font-feature-settings: normal;
+  font-variation-settings: normal;
+  font-size: 1em;
+}
+</style>
