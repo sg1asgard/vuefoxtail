bb596b1beb40a02e538cb48b3ff004f41a31da2b
George A. <sg1asgard@outlook.com>
2024-09-20 12:48:32 +0300

    buttons

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,7 +10,7 @@ import TopPart from '@/components/layout/TopPart.vue'
       <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
         <AsidePart />
         <div class="lg:pl-[19.5rem]">
-          <div class="max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16">
+          <div class="mx-auto pt-10">
             <slot></slot>
           </div>
           <FooterPart />
