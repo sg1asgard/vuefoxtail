3e06bd02041e086db8473fd47787ac76c4a9180f
George A. <sg1asgard@outlook.com>
2024-09-21 15:55:37 +0300

    updates

--- src/components/vuefoxtail/forms/buttons/ButtonVFX.vue
+++ src/components/vuefoxtail/forms/buttons/ButtonVFX.vue
@@ -13,46 +13,30 @@ const buttonsV1 = ref(
 
 // Button v2
 const btnSmall_v2 = ref(
-  `<button class="bg-indigo-200 hover:bg-indigo-300/75 rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"> Button </button>`
-)
-const btnMedium_v2 = ref(
-  `<button class="bg-indigo-200 hover:bg-indigo-300/75 rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25"> Button </button>`
-)
-const btnLarge_v2 = ref(
-  `<button class="bg-indigo-200 hover:bg-indigo-300/75 rounded-full px-6 py-2.5 text-xl hover:shadow-lg hover:shadow-black/25"> Button </button>`
+  `<button class="bg-indigo-200 hover:bg-indigo-300/75 rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"> Button </button>
+<button class="bg-indigo-200 hover:bg-indigo-300/75 rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25"> Button </button>
+<button class="bg-indigo-200 hover:bg-indigo-300/75 rounded-full px-6 py-2.5 text-xl hover:shadow-lg hover:shadow-black/25"> Button </button>`
 )
 
 // Button v3
 const btnSmall_v3 = ref(
-  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-3 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"> Button </button>`
-)
-const btnMedium_v3 = ref(
-  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-4 py-2 hover:shadow-lg hover:shadow-black/25"> Button </button>`
-)
-const btnLarge_v3 = ref(
-  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-5 py-3 text-xl hover:shadow-lg hover:shadow-black/25"> Button </button>`
+  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-3 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"> Button </button>
+<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-4 py-2 hover:shadow-lg hover:shadow-black/25"> Button </button>
+<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-5 py-3 text-xl hover:shadow-lg hover:shadow-black/25"> Button </button>`
 )
 
 // Button v4
 const btnSmall_v4 = ref(
-  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"> Button </button>`
-)
-const btnMedium_v4 = ref(
-  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25"> Button </button>`
-)
-const btnLarge_v4 = ref(
-  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25"> Button </button>`
+  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"> Button </button>
+<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25"> Button </button>
+<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25"> Button </button>`
 )
 
 // Button v5
 const btnSmall_v5 = ref(
-  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"> Button </button>`
-)
-const btnMedium_v5 = ref(
-  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25"> Button </button>`
-)
-const btnLarge_v5 = ref(
-  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25"> Button </button>`
+  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"> Button </button>
+<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25"> Button </button>
+<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25"> Button </button>`
 )
 </script>
 
@@ -107,72 +91,76 @@ const btnLarge_v5 = ref(
   <section class="mt-16">
     <!-- Button v2 -->
     <PageHeader2 sectionTitle="fill button" designVersion="bt.v2" />
-    <div class="flex flex-row space-x-6 items-center">
-      <div>
-        <button
-          class="bg-indigo-500 hover:bg-indigo-600 text-white rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"
-        >
-          Button
-        </button>
-      </div>
+    <ExampleWrapper>
+      <template v-slot:code-items>
+        <div class="flex flex-row space-x-6 items-center p-6">
+          <div>
+            <button
+              class="bg-indigo-500 hover:bg-indigo-600 text-white rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"
+            >
+              Button
+            </button>
+          </div>
 
-      <div>
-        <button
-          class="bg-indigo-500 hover:bg-indigo-600 text-white rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25"
-        >
-          Button
-        </button>
-      </div>
+          <div>
+            <button
+              class="bg-indigo-500 hover:bg-indigo-600 text-white rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25"
+            >
+              Button
+            </button>
+          </div>
 
-      <div>
-        <button
-          class="bg-indigo-500 hover:bg-indigo-600 text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25"
-        >
-          Button
-        </button>
-      </div>
-    </div>
+          <div>
+            <button
+              class="bg-indigo-500 hover:bg-indigo-600 text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25"
+            >
+              Button
+            </button>
+          </div>
+        </div>
+      </template>
 
-    <div class="py-4 flex flex-col space-y-6">
-      <ExampleWrapper>
-        <CodeWrapper lang="xml" :codeExample="btnSmall_v2" />
-      </ExampleWrapper>
-    </div>
+      <template v-slot:code-example>
+        <CodeWrapper lang="javascript" :codeExample="btnSmall_v2" />
+      </template>
+    </ExampleWrapper>
   </section>
 
   <section class="mt-16">
     <!-- Button v3 -->
     <PageHeader2 sectionTitle="fill button" designVersion="bt.v3" />
     <ExampleWrapper>
-      <div class="flex flex-row space-x-6 items-center">
-        <div>
-          <button
-            class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-3 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"
-          >
-            Small Button
-          </button>
-        </div>
+      <template v-slot:code-items>
+        <div class="flex flex-row space-x-6 items-center p-6">
+          <div>
+            <button
+              class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-3 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"
+            >
+              Small Button
+            </button>
+          </div>
 
-        <div>
-          <button
-            class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-4 py-2 hover:shadow-lg hover:shadow-black/25"
-          >
-            Medium Button
-          </button>
-        </div>
+          <div>
+            <button
+              class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-4 py-2 hover:shadow-lg hover:shadow-black/25"
+            >
+              Medium Button
+            </button>
+          </div>
 
-        <div>
-          <button
-            class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-5 py-3 text-xl hover:shadow-lg hover:shadow-black/25"
-          >
-            Large Button
-          </button>
+          <div>
+            <button
+              class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-5 py-3 text-xl hover:shadow-lg hover:shadow-black/25"
+            >
+              Large Button
+            </button>
+          </div>
         </div>
-      </div>
+      </template>
 
-      <div class="py-4 flex flex-col space-y-6">
+      <template v-slot:code-example>
         <CodeWrapper lang="xml" :codeExample="btnSmall_v3" />
-      </div>
+      </template>
     </ExampleWrapper>
   </section>
 
@@ -180,35 +168,37 @@ const btnLarge_v5 = ref(
     <!-- Button v4 -->
     <PageHeader2 sectionTitle="outline button" designVersion="bt.v4" />
     <ExampleWrapper>
-      <div class="flex flex-row space-x-6 items-center">
-        <div>
-          <button
-            class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"
-          >
-            Small Button
-          </button>
-        </div>
+      <template v-slot:code-items>
+        <div class="flex flex-row space-x-6 items-center p-6">
+          <div>
+            <button
+              class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"
+            >
+              Small Button
+            </button>
+          </div>
 
-        <div>
-          <button
-            class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25"
-          >
-            Medium Button
-          </button>
-        </div>
+          <div>
+            <button
+              class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25"
+            >
+              Medium Button
+            </button>
+          </div>
 
-        <div>
-          <button
-            class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25"
-          >
-            Large Button
-          </button>
+          <div>
+            <button
+              class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25"
+            >
+              Large Button
+            </button>
+          </div>
         </div>
-      </div>
+      </template>
 
-      <div class="py-4 flex flex-col space-y-6">
+      <template v-slot:code-example>
         <CodeWrapper lang="xml" :codeExample="btnSmall_v4" />
-      </div>
+      </template>
     </ExampleWrapper>
   </section>
 
@@ -216,35 +206,37 @@ const btnLarge_v5 = ref(
     <!-- Button v5 -->
     <PageHeader2 sectionTitle="fill button" designVersion="bt.v5" />
     <ExampleWrapper>
-      <div class="flex flex-row space-x-6 items-center">
-        <div>
-          <button
-            class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-3 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"
-          >
-            Small Button
-          </button>
-        </div>
+      <template v-slot:code-items>
+        <div class="flex flex-row space-x-6 items-center p-6">
+          <div>
+            <button
+              class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-3 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25"
+            >
+              Small Button
+            </button>
+          </div>
 
-        <div>
-          <button
-            class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-4 py-2 hover:shadow-lg hover:shadow-black/25"
-          >
-            Medium Button
-          </button>
-        </div>
+          <div>
+            <button
+              class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-4 py-2 hover:shadow-lg hover:shadow-black/25"
+            >
+              Medium Button
+            </button>
+          </div>
 
-        <div>
-          <button
-            class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-5 py-3 text-xl hover:shadow-lg hover:shadow-black/25"
-          >
-            Large Button
-          </button>
+          <div>
+            <button
+              class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-5 py-3 text-xl hover:shadow-lg hover:shadow-black/25"
+            >
+              Large Button
+            </button>
+          </div>
         </div>
-      </div>
+      </template>
 
-      <div class="py-4 flex flex-col space-y-6">
+      <template v-slot:code-example>
         <CodeWrapper lang="xml" :codeExample="btnSmall_v5" />
-      </div>
+      </template>
     </ExampleWrapper>
   </section>
 </template>
