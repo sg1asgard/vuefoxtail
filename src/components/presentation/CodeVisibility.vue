5462cad8a8b32e647bcfd494c34dfaf8e2cdf050
George A. <sg1asgard@outlook.com>
2024-09-21 13:00:18 +0300

    updates

--- src/components/presentation/CodeVisibility.vue
+++ src/components/presentation/CodeVisibility.vue
@@ -1,17 +1,6 @@
-<script setup>
-import { ref } from 'vue'
-
-const codeVisibility = ref(true)
-</script>
+<script setup></script>
 <template>
   <div class="">
-    <div @click="codeVisibility = !codeVisibility">Show code</div>
-    <div :class="`${!codeVisibility ? 'hidden' : 'block'}`">
-      <pre>
-        <code class="language-css">
-          <slot></slot>
-        </code>
-      </pre>
-    </div>
+    <slot></slot>
   </div>
 </template>
