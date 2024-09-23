c5c93576b255b015f29fbc6211e9ecada299767c
George A. <sg1asgard@outlook.com>
2024-09-23 16:16:06 +0300

    updates

--- src/components/presentation/CodeExample.vue
+++ src/components/presentation/CodeExample.vue
@@ -31,7 +31,34 @@ const js = ref(false)
 </script>
 
 <template>
-  <CodeWrapper v-if="html" lang="xml" :codeExample="props.codeHTML" />
-  <CodeWrapper v-if="css" lang="css" :codeExample="props.codeCSS" />
-  <CodeWrapper v-if="js" lang="javascript" :codeExample="props.codeJS" />
+  <div class="flex flex-col space-y-6 p-6">
+    <div class="flex flex-row px-6 py-4 space-x-2 border-t-[1px] border-b-[1px]">
+      <div
+        v-if="props.isHTML"
+        @click="$emit('showHTML')"
+        class="bg-indigo-100 px-4 py-1 rounded-md cursor-pointer"
+      >
+        HTML
+      </div>
+      <div
+        v-if="props.isCSS"
+        @click="$emit('showCSS')"
+        class="px-4 py-1 rounded-md hover:bg-indigo-100 cursor-pointer"
+      >
+        CSS
+      </div>
+      <div
+        v-if="props.isJS"
+        @click="$emit('showJS')"
+        class="px-4 py-1 rounded-md hover:bg-indigo-100 cursor-pointer"
+      >
+        JavaScript
+      </div>
+    </div>
+    <div class="flex flex-col space-y-6 p-6">
+      <CodeWrapper v-if="html" lang="xml" :codeExample="props.codeHTML" />
+      <CodeWrapper v-if="css" lang="css" :codeExample="props.codeCSS" />
+      <CodeWrapper v-if="js" lang="javascript" :codeExample="props.codeJS" />
+    </div>
+  </div>
 </template>
--- src/components/presentation/ExampleWrapper.vue
+++ src/components/presentation/ExampleWrapper.vue
@@ -1,51 +1,7 @@
-<script setup>
-import { ref } from 'vue'
-const props = defineProps({
-  isHtml: {
-    type: Boolean,
-    required: false
-  },
-  isCss: {
-    type: Boolean,
-    required: false
-  },
-  isJs: {
-    type: Boolean,
-    required: false
-  }
-})
-</script>
+<script setup></script>
 <template>
   <div class="border-[1px] rounded-xl">
     <slot name="code-items"></slot>
-    <div
-      v-if="isHtml || isCss || isJs"
-      class="flex flex-row px-6 py-4 space-x-2 border-t-[1px] border-b-[1px]"
-    >
-      <div
-        v-if="isHtml"
-        @click="$emit('showHTML')"
-        class="bg-indigo-100 px-4 py-1 rounded-md cursor-pointer"
-      >
-        HTML
-      </div>
-      <div
-        v-if="isCss"
-        @click="$emit('showCSS')"
-        class="px-4 py-1 rounded-md hover:bg-indigo-100 cursor-pointer"
-      >
-        CSS
-      </div>
-      <div
-        v-if="isJs"
-        @click="$emit('showJS')"
-        class="px-4 py-1 rounded-md hover:bg-indigo-100 cursor-pointer"
-      >
-        JavaScript
-      </div>
-    </div>
-    <div class="flex flex-col space-y-6 p-6">
-      <slot name="code-example"></slot>
-    </div>
+    <slot name="code-example"></slot>
   </div>
 </template>
