314b99868fd44cea28445b7cffea6417c2266205
George A. <sg1asgard@outlook.com>
2024-09-23 14:33:54 +0300

    updates

--- src/components/vuefoxtail/forms/buttons/CheckboxVFX.vue
+++ src/components/vuefoxtail/forms/buttons/CheckboxVFX.vue
@@ -6,103 +6,11 @@ import PageHeader3 from '@/components/presentation/PageHeader3.vue'
 import ExampleWrapper from '@/components/presentation/ExampleWrapper.vue'
 
 // Button v1
-const buttonsV1_fill = ref(
-  `<button class="bg-indigo-200 hover:bg-indigo-300/75 rounded-md px-3 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150">Button</button>
-<button class="bg-indigo-200 hover:bg-indigo-300/75 rounded-md px-4 py-2 hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-indigo-200 hover:bg-indigo-300/75 rounded-md px-5 py-2 hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-
-// with icon
-<button class="inline-flex flex-row items-center space-x-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-md px-5 py-2.5 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150">
-  <span class="grow">With Icon</span>
-  <span class="icon-[ph--heart-light]"></span>
-</button>`
-)
-
-const buttonsV1_gradient = ref(
-  `<button class="bg-gradient-to-t from-indigo-500 to-indigo-400 hover:from-indigo-600 hover:to-indigo-500 text-white rounded-md px-3 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25 inline-block transition-all ease-in-out duration-150">Button</button>
-<button class="bg-indigo-500 hover:bg-indigo-600 text-white rounded-md px-4 py-2 hover:shadow-lg hover:shadow-black/25 inline-block transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-indigo-500 hover:bg-indigo-600 text-white rounded-md px-5 py-2.5 text-xl hover:shadow-lg hover:shadow-black/25 inline-block transition-all ease-in-out duration-150"> Button </button>
-
-// with icon
-<button class="inline-flex flex-row items-center space-x-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-md px-5 py-2.5 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150">
-  <span class="grow">With Icon</span>
-  <span class="icon-[ph--heart-light]"></span>
-</button>`
-)
-
-// Button v2
-const btnSmallV2_fill = ref(
-  `<button class="bg-indigo-200 hover:bg-indigo-300/75 rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-indigo-200 hover:bg-indigo-300/75 rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-indigo-200 hover:bg-indigo-300/75 rounded-full px-6 py-2.5 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-
-// with icon
-<button class="inline-flex flex-row items-center space-x-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150">
-  <span class="grow">With Icon</span>
-  <span class="icon-[ph--heart-light]"></span>
-</button>`
-)
-
-const btnSmallV2_gradient = ref(
-  `<button class="bg-gradient-to-t from-indigo-500 to-indigo-400 hover:from-indigo-600 hover:to-indigo-500 text-white rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-gradient-to-t from-indigo-500 to-indigo-400 hover:from-indigo-600 hover:to-indigo-500 text-white rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-gradient-to-t from-indigo-500 to-indigo-400 hover:from-indigo-600 hover:to-indigo-500 text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-
-// with icon
-<button class="inline-flex flex-row items-center space-x-2 bg-gradient-to-t from-indigo-500 to-indigo-400 hover:from-indigo-600 hover:to-indigo-500 text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150">
-  <span class="grow">With Icon</span>
-  <span class="icon-[ph--heart-light]"></span>
-</button>`
-)
-
-// Button v3
-const btnSmallV3_fill = ref(
-  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-3 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-4 py-2 hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-5 py-3 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-
-// with icon
-<button class="inline-flex flex-row items-center space-x-2 bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-md px-5 py-3 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150">
-  <span class="grow">With Icon</span>
-  <span class="icon-[ph--heart-light]"></span>
-</button>`
-)
-
-const btnSmallV3_gradient = ref(
-  `<button class="bg-gradient-to-t from-slate-200 to-white hover:from-indigo-500 hover:to-indigo-500/75 border-2 border-indigo-500 hover:text-white rounded-md px-3 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-gradient-to-t from-slate-200 to-white hover:from-indigo-500 hover:to-indigo-500/75 border-2 border-indigo-500 hover:text-white rounded-md px-4 py-2 hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-gradient-to-t from-slate-200 to-white hover:from-indigo-500 hover:to-indigo-500/75 border-2 border-indigo-500 hover:text-white rounded-md px-5 py-3 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-
-// with icon
-<button class="inline-flex flex-row items-center space-x-2 bg-gradient-to-t from-slate-200 to-white hover:from-indigo-500 hover:to-indigo-500/75 border-2 border-indigo-500 hover:text-white rounded-md px-5 py-3 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150">
-  <span class="grow">With Icon</span>
-  <span class="icon-[ph--heart-light]"></span>
-</button>`
-)
-
-// Button v4
-const btnSmallV4_fill = ref(
-  `<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-3.5 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-5 py-2 hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-
-// with icon
-<button class="inline-flex flex-row items-center space-x-2 bg-white hover:bg-indigo-500 border-2 border-indigo-500 hover:text-white rounded-full px-6 py-3 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150">
-  <span class="grow">With Icon</span>
-  <span class="icon-[ph--heart-light]"></span>
-</button>`
-)
-
-const btnSmallV4_gradient = ref(
-  `<button class="bg-gradient-to-t from-slate-200 to-white hover:from-indigo-500 hover:to-indigo-500/75 border-2 border-indigo-500 hover:text-white rounded-full px-3 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-gradient-to-t from-slate-200 to-white hover:from-indigo-500 hover:to-indigo-500/75 border-2 border-indigo-500 hover:text-white rounded-full px-4 py-2 hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-<button class="bg-gradient-to-t from-slate-200 to-white hover:from-indigo-500 hover:to-indigo-500/75 border-2 border-indigo-500 hover:text-white rounded-full px-5 py-3 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"> Button </button>
-
-// with icon
-<button class="inline-flex flex-row items-center space-x-2 bg-gradient-to-t from-slate-200 to-white hover:from-indigo-500 hover:to-indigo-500/75 border-2 border-indigo-500 hover:text-white rounded-full px-5 py-3 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150">
-  <span class="grow">With Icon</span>
-  <span class="icon-[ph--heart-light]"></span>
-</button>`
+const checkboxV1_small = ref(
+  `<div class="flex flex-row items-center space-x-2">
+  <input class="rounded-[4px] p-2" type="checkbox" value="" id="flexCheckDefault" />
+  <label class="" for="flexCheckDefault">Default</label>
+</div>`
 )
 </script>
 
@@ -113,34 +21,67 @@ const btnSmallV4_gradient = ref(
       add the icon to the rest of the sizes as show in the examples.
     </div>
     <!-- Button v1 -->
-    <!-- Fill -->
+    <!-- Small -->
     <PageHeader2 sectionTitle="button v1" />
-    <PageHeader3 sectionTitle="Fill Color" />
+    <PageHeader3 sectionTitle="Small" />
     <ExampleWrapper>
       <template v-slot:code-items>
         <div class="items-center px-6 py-4 space-y-2">
+          <div class="inline-flex me-6">
+            <div class="flex flex-row items-center space-x-2">
+              <input class="rounded-[4px] p-2" type="checkbox" value="" id="flexCheckDefault" />
+              <label class="" for="flexCheckDefault">Default</label>
+            </div>
+          </div>
+
           <div class="inline-flex me-2">
-            <button
-              class="bg-indigo-500 hover:bg-indigo-600 text-white rounded-md px-3 py-1.5 text-sm hover:shadow-lg hover:shadow-black/25 inline-block transition-all ease-in-out duration-150"
-            >
-              Small Button
-            </button>
-          </div>
-
-          <div class="">
-            <input class="rounded-full" type="checkbox" value="" id="flexCheckDefault" />
-            <label class="" for="flexCheckDefault"> Default checkbox </label>
-          </div>
-
-          <div class="">
-            <input class="" type="checkbox" value="" id="flexCheckChecked" checked />
-            <label class="" for="flexCheckChecked"> Checked checkbox </label>
+            <div class="flex flex-row items-center space-x-2">
+              <input
+                class="rounded-[4px] p-2"
+                type="checkbox"
+                value=""
+                id="flexCheckChecked"
+                checked
+              />
+              <label class="" for="flexCheckChecked">Checked</label>
+            </div>
           </div>
         </div>
       </template>
 
       <template v-slot:code-example>
-        <CodeWrapper lang="javascript" :codeExample="buttonsV1_fill" />
+        <CodeWrapper lang="javascript" :codeExample="checkboxV1_small" />
+      </template>
+    </ExampleWrapper>
+
+    <PageHeader3 sectionTitle="Large" class="mt-8" />
+    <ExampleWrapper>
+      <template v-slot:code-items>
+        <div class="items-center px-6 py-4 space-y-2">
+          <div class="inline-flex me-6">
+            <div class="flex flex-row items-center space-x-2">
+              <input class="rounded-[4px] p-2.5" type="checkbox" value="" id="flexCheckDefault" />
+              <label class="text-xl" for="flexCheckDefault">Default</label>
+            </div>
+          </div>
+
+          <div class="inline-flex me-6">
+            <div class="flex flex-row items-center space-x-2">
+              <input
+                class="rounded-[4px] p-2.5"
+                type="checkbox"
+                value=""
+                id="flexCheckChecked"
+                checked
+              />
+              <label class="text-xl" for="flexCheckDefault">Checked</label>
+            </div>
+          </div>
+        </div>
+      </template>
+
+      <template v-slot:code-example>
+        <CodeWrapper lang="javascript" :codeExample="checkboxV1_small" />
       </template>
     </ExampleWrapper>
   </section>
