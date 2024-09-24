3c2cd35043fcddf2fe2efa5739531e3e4a15a350
George A. <sg1asgard@outlook.com>
2024-09-24 17:30:54 +0300

    general info

--- src/components/layout/FooterPart.vue
+++ src/components/layout/FooterPart.vue
@@ -16,7 +16,51 @@ const currentYear = ref(new Date())
         Copyright © 2024 - {{ currentYear.getFullYear() }} Project Hive
       </div>
       <div class="my-8">
-        Join us - <a :href="discord.url">{{ discord.name }}</a>
+        <div class="flex flex-row space-x-4">
+          <a
+            :href="discord.url"
+            class="inline-flex space-x-2 items-center px-3 py-1.5 rounded-lg border-[1px] border-slate-300 hover:border-slate-700 hover:bg-slate-700 hover:text-white transition-all duration-150 ease-in-out"
+            ><span>{{ discord.name }}</span
+            ><span class="icon-[ph--discord-logo-fill] size-6"></span
+          ></a>
+
+          <a
+            :href="discord.url"
+            class="inline-flex space-x-2 items-center px-3 py-1.5 rounded-lg border-[1px] border-slate-300 hover:border-slate-700 hover:bg-slate-700 hover:text-white transition-all duration-150 ease-in-out"
+            ><span>{{ telegram.name }}</span
+            ><span class="icon-[ph--telegram-logo-fill] size-6"></span
+          ></a>
+        </div>
+      </div>
+
+      <div class="my-6 text-center">
+        <div>We ❤️ and support Open Source!</div>
+        <div class="mt-4 flex space-x-8 items-center">
+          <a href="https://cachyos.org" target="_blank" class="inline-flex items-center">
+            <img
+              src="https://cachyos.org/_astro/logo.DVTdAJi6.svg"
+              alt="CachyOS - https://cachyos.org"
+              class="size-12"
+            />
+            <span class="text-2xl font-bold ms-4">CachyOS</span>
+          </a>
+
+          <a href="https://fedoraproject.org" target="_blank" class="inline-flex items-center">
+            <img
+              src="https://fedoraproject.org/assets/images/logos/fedora-blue.png"
+              alt="Fedora Project - https://fedoraproject.org"
+              class="max-h-10"
+            />
+          </a>
+
+          <a href="https://itsfoss.com" target="_blank" class="inline-flex items-center">
+            <img
+              src="https://itsfoss.com/content/images/size/w300/format/webp/2023/01/itsfoss-logo.png"
+              alt="IT'S FOSS - https://itsfoss.com"
+              class="max-h-12"
+            />
+          </a>
+        </div>
       </div>
     </div>
   </footer>
