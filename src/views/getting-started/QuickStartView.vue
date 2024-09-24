4fbfaeef6dd9e4dc497bc734f67a9aa93c59a920
George A. <sg1asgard@outlook.com>
2024-09-24 16:19:35 +0300

    general info

--- src/views/getting-started/QuickStartView.vue
+++ src/views/getting-started/QuickStartView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import MainPart from '@/components/layout/MainPart.vue'
 import PageHeader from '@/components/presentation/PageHeader.vue'
+import PageHeader2 from '@/components/presentation/PageHeader2.vue'
 import MainLayout from '@/layouts/MainLayout.vue'
 </script>
 
@@ -33,6 +34,34 @@ import MainLayout from '@/layouts/MainLayout.vue'
             🎉
           </li>
         </ol>
+      </section>
+
+      <section class="mt-12">
+        <PageHeader2 sectionTitle="So, what's next?" />
+        <p>Go checkout the built components and see what you can use in your next project.</p>
+
+        <p class="mt-4">
+          If you have any questions we have a community over Discord and Telegram. Come join us!
+        </p>
+
+        <div class="flex flex-row items-center">
+          <div>
+            <button
+              class="inline-flex flex-row items-center space-x-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-md px-4 py-2 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"
+            >
+              <span class="grow">Discord</span>
+              <span class="icon-[ph--discord-logo-fill] size-7"></span>
+            </button>
+          </div>
+          <div>
+            <button
+              class="inline-flex flex-row items-center space-x-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-md px-4 py-2 text-xl hover:shadow-lg hover:shadow-black/25 transition-all ease-in-out duration-150"
+            >
+              <span class="grow">Telegram</span>
+              <span class="icon-[ph--telegram-logo] size-7"></span>
+            </button>
+          </div>
+        </div>
       </section>
     </MainPart>
   </MainLayout>
