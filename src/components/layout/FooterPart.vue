3005492f29297641138695d2c030848f1ecb21a2
George A. <sg1asgard@outlook.com>
2024-09-24 16:48:30 +0300

    general info

--- src/components/layout/FooterPart.vue
+++ src/components/layout/FooterPart.vue
@@ -5,16 +5,19 @@ const currentYear = ref(new Date())
 </script>
 <template>
   <footer class="mt-12 pt-10 pb-28 text-slate-500 border-t-[1px] border-slate-900/10">
-    <div class="sm:flex justify-between">
-      <p>
+    <div class="flex flex-col items-center">
+      <div>
         Released under the
         <a class="vt-link link link" :href="license.url" target="_blank" rel="noopener noreferrer"
           >{{ license.name }} License</a
         >.
-      </p>
-      <p class="copyright">Copyright © 2024 - {{ currentYear.getFullYear() }} George Antohi</p>
+      </div>
+      <div class="copyright my-2">
+        Copyright © 2024 - {{ currentYear.getFullYear() }} Project Hive
+      </div>
+      <div class="my-8">
+        Join us - <a :href="discord.url">{{ discord.name }}</a>
+      </div>
     </div>
-
-    <div>asdas</div>
   </footer>
 </template>
