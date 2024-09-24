653f8a2e588e72c2a98e490e961d46286fa53cf0
George A. <sg1asgard@outlook.com>
2024-09-24 16:32:14 +0300

    general info

--- src/components/layout/FooterPart.vue
+++ src/components/layout/FooterPart.vue
@@ -7,12 +7,14 @@ const currentYear = ref(new Date())
   <footer
     class="mt-12 pt-10 pb-28 sm:flex justify-between text-slate-500 border-t-[1px] border-slate-900/10"
   >
-    <p>
-      Released under the
-      <a class="vt-link link link" :href="license.url" target="_blank" rel="noopener noreferrer"
-        >{{ license.name }} License</a
-      >.
-    </p>
-    <p class="copyright">Copyright © 2024 - {{ currentYear.getFullYear() }} George Antohi</p>
+    <div>
+      <p>
+        Released under the
+        <a class="vt-link link link" :href="license.url" target="_blank" rel="noopener noreferrer"
+          >{{ license.name }} License</a
+        >.
+      </p>
+      <p class="copyright">Copyright © 2024 - {{ currentYear.getFullYear() }} George Antohi</p>
+    </div>
   </footer>
 </template>
