34b4643deb350890660418209deb0d840286ca98
George A. <sg1asgard@outlook.com>
2024-09-24 15:19:04 +0300

    general info

--- src/views/getting-started/IntroductionView.vue
+++ src/views/getting-started/IntroductionView.vue
@@ -16,7 +16,23 @@ import MainLayout from '@/layouts/MainLayout.vue'
 
       <section class="mb-8">
         <PageHeader2 :sectionTitle="`Why use ${appName}`" />
-        <p></p>
+        <p class="mb-4">
+          As coders and designers we like to reuse features without the need to rebuild from scratch
+          components and designs every single time we start a brand new project.
+        </p>
+        <p class="mb-4">
+          Now we have the option to reuse already made components! Isn't that sweet? 🎉
+        </p>
+      </section>
+
+      <section class="mb-8">
+        <PageHeader2 :sectionTitle="`Built on Tailwind CSS`" />
+        <p class="mb-4">
+          We chose Tailwind CSS due to it flexibility and modularity. It has a great design system
+          with sizes, colors, typography, shadows, responsiveness and many more other attributes and
+          properties. It is easy to install, use and configure and in generally comes in small
+          factor 10 to 20 kb for most of the projects.
+        </p>
       </section>
     </MainPart>
   </MainLayout>
