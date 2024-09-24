38a078d5bed8bf30a8f11d09f174f992f9a3f648
George A. <sg1asgard@outlook.com>
2024-09-24 16:33:11 +0300

    general info

--- src/components/layout/FooterPart.vue
+++ src/components/layout/FooterPart.vue
@@ -4,10 +4,8 @@ import { ref } from 'vue'
 const currentYear = ref(new Date())
 </script>
 <template>
-  <footer
-    class="mt-12 pt-10 pb-28 sm:flex justify-between text-slate-500 border-t-[1px] border-slate-900/10"
-  >
-    <div>
+  <footer class="mt-12 pt-10 pb-28 text-slate-500 border-t-[1px] border-slate-900/10">
+    <div class="sm:flex justify-between">
       <p>
         Released under the
         <a class="vt-link link link" :href="license.url" target="_blank" rel="noopener noreferrer"
@@ -16,5 +14,7 @@ const currentYear = ref(new Date())
       </p>
       <p class="copyright">Copyright © 2024 - {{ currentYear.getFullYear() }} George Antohi</p>
     </div>
+
+    <div>asdas</div>
   </footer>
 </template>
