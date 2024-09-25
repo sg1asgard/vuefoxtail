8f05acc2604b6d8a35507e7bfef692c6fd9b9545
George A. <sg1asgard@outlook.com>
2024-09-25 14:50:48 +0300

    input

--- src/components/vuefoxtail/forms/InputVFX.vue
+++ src/components/vuefoxtail/forms/InputVFX.vue
@@ -6,34 +6,16 @@ import CodeExample from '@/components/presentation/CodeExample.vue'
 import AlertWrapper from '@/components/presentation/alerts/AlertWrapper.vue'
 
 // Select v1
-const selectV1_common_css = ref(
+const inputV1_common_css = ref(
   `// add this once in your main.css
 
-select:disabled {
+input[type="text"]:disabled {
   background-color: var(--vfox-c-text-disabled);
 }`
 )
 
-const selectV1_small = ref(
+const inputV1_small = ref(
   `<select class="w-full bg-white border-2 border-slate-300 rounded-md px-3 py-2 text-sm transition-all ease-in-out duration-150 active:shadow-lg">
-  <option selected>Open this select menu</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</select>`
-)
-
-const selectV1_medium = ref(
-  `<select class="w-full bg-white border-2 border-slate-300 rounded-md px-4 p-2.5 transition-all ease-in-out duration-150 active:shadow-lg">
-  <option selected>Open this select menu</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</select>`
-)
-
-const selectV1_large = ref(
-  `<select class="w-full bg-white border-2 border-slate-300 rounded-md text-lg px-4 p-3 transition-all ease-in-out duration-150 active:shadow-lg">
   <option selected>Open this select menu</option>
   <option value="1">One</option>
   <option value="2">Two</option>
@@ -77,60 +59,8 @@ const selectV1_large = ref(
 
       <template v-slot:code-example>
         <CodeExample
-          :codeHTML="selectV1_small"
-          :codeCSS="selectV1_common_css"
-          :isHTML="true"
-          :isCSS="true"
-        />
-      </template>
-    </ExampleWrapper>
-
-    <!-- Medium -->
-    <PageHeader3 sectionTitle="Medium" class="mt-8" />
-    <ExampleWrapper isHtml isCss isJs>
-      <template v-slot:code-items>
-        <div class="items-center px-6 py-4 space-y-2">
-          <select
-            class="w-full bg-white border-2 border-slate-300 rounded-md px-4 p-2.5 transition-all ease-in-out duration-150 active:shadow-lg"
-          >
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-        </div>
-      </template>
-
-      <template v-slot:code-example>
-        <CodeExample
-          :codeHTML="selectV1_medium"
-          :codeCSS="selectV1_common_css"
-          :isHTML="true"
-          :isCSS="true"
-        />
-      </template>
-    </ExampleWrapper>
-
-    <!-- Large -->
-    <PageHeader3 sectionTitle="Medium" class="mt-8" />
-    <ExampleWrapper isHtml isCss isJs>
-      <template v-slot:code-items>
-        <div class="items-center px-6 py-4 space-y-2">
-          <select
-            class="w-full bg-white border-2 border-slate-300 rounded-md text-lg px-4 p-3 transition-all ease-in-out duration-150 active:shadow-lg"
-          >
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-        </div>
-      </template>
-
-      <template v-slot:code-example>
-        <CodeExample
-          :codeHTML="selectV1_large"
-          :codeCSS="selectV1_common_css"
+          :codeHTML="inputV1_small"
+          :codeCSS="inputV1_common_css"
           :isHTML="true"
           :isCSS="true"
         />
