38eaf1cce1707bbc6fd10d1bed451c9683a531c8
George A. <sg1asgard@outlook.com>
2024-09-25 10:01:35 +0300

    checkbox updates

--- src/components/presentation/alerts/AlertWrapper.vue
+++ src/components/presentation/alerts/AlertWrapper.vue
@@ -1,20 +1,26 @@
 <script setup>
-import { reactive } from 'vue'
+import { computed } from 'vue'
 
 const props = defineProps({
-  info: Boolean,
-  warning: Boolean,
-  danger: Boolean
+  variant: {
+    type: String,
+    validator: (value) => ['info', 'warning', 'danger'].includes(value),
+    required: true
+  }
 })
 
-const alert = reactive({
-  info: 'border-sky-300 bg-sky-100'
+const alertClasses = computed(() => {
+  const classes = {
+    info: 'border-sky-300 bg-sky-100',
+    warning: 'border-amber-300 bg-amber-100',
+    danger: 'border-rose-300 bg-rose-100'
+  }
+  return classes[props.variant] || ''
 })
 </script>
+
 <template>
-  <p
-    :class="`p-4 border-s-4 ${props.info ? alert.info : ''} ${props.warning ? alert.warning : ''} ${props.danger ? alert.danger : ''}`"
-  >
+  <p :class="`p-4 border-s-4 ${alertClasses}`">
     <slot></slot>
   </p>
 </template>
